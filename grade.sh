9935879489fae24c09e162d83855cac4ee81e76f
MichaelPena7 <122485247+MichaelPena7@users.noreply.github.com>
2023-03-01 12:15:20 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -31,6 +31,8 @@ fi
 
 cd .. 
 
+cp student-submission/ListExamples.java ./
+
 javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
 echo 'compiling JUnit...'
 java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples.java
