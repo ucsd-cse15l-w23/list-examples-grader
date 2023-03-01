10cf6511c6f4a254869d0ebc1cdbb46b1cab0d9f
jliu312 <85335934+jliu312@users.noreply.github.com>
2023-03-01 15:58:29 -0800

    ljklk

--- grade.sh
+++ grade.sh
@@ -4,6 +4,35 @@ rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
 
-if [[-f ListExamples.java]]
+cd student-submission
+
+if [[ -f ListExamples.java ]]
 then
-    
+    echo 'ListExamples.java exists'
+else 
+    echo 'ListExamples.java does not exist'
+    exit
+fi
+
+cp ../TestListExamples.java .
+cp -r ../lib .
+
+javac -cp $CPATH *.java
+echo 'Finished compiling'
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > ../output.txt
+echo 'Finished running tests'
+
+if [[ `grep -h "OK" ../output.txt` != "" ]] 
+then
+    echo 'All tests passed'
+else
+    TESTS=`grep -h "Tests run" ../output.txt`
+    echo $TESTS
+fi
+
+
+
+# rm -rf student-submission
+# echo 'Finished cleaning up'
+
